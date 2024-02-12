f43d1f6bc6d784c6e8a4f4fbbaa5f90d801882a7
Raj Rasane <rasaneraj04@gmail.com>
2024-02-12 19:05:54 +0530

    new notes added

---
+++ JS notes/alert_and_promt.js
@@ -0,0 +1,14 @@
+// 1. Alert 
+// - displays an alert message on the page
+
+// syntax
+alert("this is a alert");
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// 2. Prompt
+// - displays a dialog box that asks user for some input
+// - usually used to get input from user
+
+// syntax
+prompt("Enter Your Name :");
---
+++ JS notes/error_and_warning_through_console.js
@@ -0,0 +1,13 @@
+// 1. Error 
+// - displays an error in console
+
+// syntax 
+console.error("This Displays as an error in console");
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// 2. Warning
+// -displays an warning in console
+
+// syntax
+console.warn("This Displays as an warning in console");
