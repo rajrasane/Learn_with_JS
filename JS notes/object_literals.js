7e70f2b120674a1b583ee635dd30902e5588e9bb
Raj Rasane <rasaneraj04@gmail.com>
2024-02-27 10:19:05 +0530

    new notes added

---
+++ JS notes/object_literals.js
@@ -0,0 +1,19 @@
+// Object Literals
+
+// - we generally make objects constant while declaring
+// - used to store keyed collections and complex entities
+
+// property => (key,value)pair
+
+// - objects are collections of properties
+
+// for e.g
+const student = {
+    name : "Raj",
+    age : 19,
+    gender : "Male",
+    class : "FYBCS",
+    Marks : 9.55
+};
+
+let stud = ["Raj",19,"Male","FYBCS",9.55];
