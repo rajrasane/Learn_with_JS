004167eb859fd6ac6ac9f40db319395e0fdc0447
Raj Rasane <rasaneraj04@gmail.com>
2024-02-28 08:58:09 +0530

    practice project created

---
+++ JS notes/twitterpost_usingObjectLiterals.js
@@ -0,0 +1,15 @@
+// - In this we create an object literal for the properties of twitter post which includes
+
+// - username
+// - content
+// - likes
+// - reposts
+// - tags
+
+const post = {
+    username : "@rajrasane04",
+    content : "This is my first tweet",
+    likes : 143,
+    reposts : 4,
+    tags : ["@foryou","@elonmusk"]
+};
