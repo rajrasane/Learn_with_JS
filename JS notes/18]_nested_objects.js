d79fdf03070742526609b7654452c860e997ec66
Raj Rasane <rasaneraj04@gmail.com>
2024-02-28 09:46:31 +0530

    new notes added

---
+++ JS notes/18]_nested_objects.js
@@ -0,0 +1,19 @@
+// - we can create multiple objects in a single object
+
+// - for e.g
+const friends = {
+    raj : {
+        marks : 9.55,
+        city : "sangamner",
+    },
+
+    om : {
+        marks : 9.18,
+        city : "akole"
+    },
+
+    onkar : {
+        marks : 9.00,
+        city : "Pune"
+    }
+};
