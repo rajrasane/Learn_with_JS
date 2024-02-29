2508c9a3dc6d8ddfbd32ab2dd3c5b5f3992d9bdd
Raj Rasane <rasaneraj04@gmail.com>
2024-02-29 23:06:31 +0530

    new notes added

--- JS notes/21]_Functions.js
+++ JS notes/21]_Functions.js
@@ -13,28 +13,79 @@
 //---------------------------------------------------------------------------------------------------------------------------------------
 
 // for e.g
-function helloworld(){
-    console.log("Hello World!");
-}
+// function helloworld(){
+//     console.log("Hello World!");
+// }
 
-helloworld();
+// helloworld();
 
 
-function print1to5(){
-    for(let i = 1;i<=5;i++){
-        console.log(i);
-    }
-}
+// function print1to5(){
+//     for(let i = 1;i<=5;i++){
+//         console.log(i);
+//     }
+// }
 
-print1to5();
+// print1to5();
 
-function isadult(){
-    let age = prompt("Enter your age");
-    if(age<18){
-        console.log("You are not a Adult");
+// function isadult(){
+//     let age = prompt("Enter your age");
+//     if(age<18){
+//         console.log("You are not a Adult");
+//     }else{
+//         console.log("You are a Adult")
+//     }
+// }
+
+// isadult();
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+//                       Function with arguments
+
+//  function fname(arg1,agr2){
+        // do something
+// }
+
+// for e.g
+
+// function yourname(name,age){
+//     console.log(`${name}'s ages is ${age}`);
+// };
+
+// yourname("raj",18);
+
+// for e.g
+
+// function addn(a,b){
+//     let sum = a+b;
+//     console.log(`Addition of ${a} and ${b} is ${sum}`)
+// };
+
+// addn(41243,54345)
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+//                        Return Keyword 
+
+// - Anything after the return statement will not be executed
+
+function subn(a,b){
+    return a-b;
+    console.log(sum);
+};
+
+// Method 1 : console.log(subn(13,5));
+
+// Method 2 :  console.log(subn(subn(13,6),5))
+
+// for e.g
+function chkadult(age){
+    if(age>=18){
+        return "Adult";
     }else{
-        console.log("You are a Adult")
+        return "Not Adult";
     }
-}
+};
 
-isadult();
+console.log(chkadult(3))
