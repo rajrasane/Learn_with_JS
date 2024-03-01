3ccd17f83ef040015705ae86d8509f7ddcdc14fe
Raj Rasane <rasaneraj04@gmail.com>
2024-03-01 10:06:29 +0530

    new notes added

---
+++ JS notes/23]_Methods.js
@@ -0,0 +1,39 @@
+//                                          Methods 
+
+// - Methods are the actions that can be performed on an object
+
+// for E.g
+// Making Calulator 
+
+const calculator = {
+    add : function(a,b){
+        console.log(a+b);
+    },
+    sub : function(a,b){
+        console.log(a-b);
+    },
+    mul : function(a,b){
+        console.log(a*b);
+    },
+    div : function(a,b){
+        console.log(a/b);
+    },
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+//                                     Methods Shorthand
+
+// for e.g
+
+const calci = {
+    add(a,b){
+        return a+b;
+    },
+    sub(a,b){
+        return a-b;
+    }
+}
+
