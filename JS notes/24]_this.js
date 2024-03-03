bf6c518aa23cd3172441820089f6f3f6dbd549a6
Raj Rasane <rasaneraj04@gmail.com>
2024-03-03 09:22:53 +0530

    new notes added

---
+++ JS notes/24]_this.js
@@ -0,0 +1,18 @@
+// this Keyword
+
+// - this keyword refers to an object that is executing the current piece of code
+
+// for e.g
+const student = {
+    name : "Raj",
+    phy : 60,
+    math : 38,
+    chem : 58,
+    bio : 53,
+    eng : 69,
+    sansk : 75,
+    getavg(){
+        let avg = (this.phy+this.math+this.chem+this.bio+this.eng+this.sansk)/6;
+        console.log(`${this.name} got average marks ${avg}`);
+    }
+};
