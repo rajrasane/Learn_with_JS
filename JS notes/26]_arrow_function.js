cd65e7311ac08a4abb728f12bad33dbb1f32d7c3
Raj Rasane <rasaneraj04@gmail.com>
2024-03-04 14:46:46 +0530

    notes updated

--- JS notes/26]_arrow_function.js
+++ JS notes/26]_arrow_function.js
@@ -23,3 +23,39 @@ const printhello = () => {
 const mulpn = (a,b) => (
     a*b
 );  
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// 'this' with arrow functions
+
+// - when we use this keyword for a normal function , it is consider as a global scope 
+// - when we use this keyword for a arrow function , it is consider as a parent scope or lexical scope
+
+// for e.g
+const student = {
+    name : "Raj",
+    marks : 9.55,
+    property : this,// global scope <= window object
+    getName : function () {
+        console.log(this.name); // lexical scope <= student object
+    },
+    getMarks : () => {
+        console.log(this);
+        return this.marks; // parent's scope <= window object
+    },
+    
+    // benefits
+    getInfo1 : function() {
+        setTimeout( () => {
+            console.log(this); // student object
+        },2000 )
+    },
+    getInfo2 : function() {
+        setTimeout( function() {
+            console.log(this); // window object
+        },2000 )
+    }
+};
+
+// i.e here in this e.g for normal function this = student
+// and for a arrow function this = window
