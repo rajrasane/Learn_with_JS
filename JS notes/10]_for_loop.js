fe17747e082875dabe4281c187456f8cdf78dbcf
Raj Rasane <rasaneraj04@gmail.com>
2024-03-16 13:02:20 +0530

    notes updated

--- JS notes/10]_for_loop.js
+++ JS notes/10]_for_loop.js
@@ -1,36 +1,36 @@
-// for(let i =1;i <= 5;i++){
-//     console.log(i);
-// }
+for(let i =1;i <= 5;i++){
+    console.log(i);
+}
 
-// console.log("Backwards");
+console.log("Backwards");
 
-// for(let i =5;i >= 1;i--){
-//     console.log(i);
-// }
+for(let i =5;i >= 1;i--){
+    console.log(i);
+}
 
-// console.log("Even Numbers");
+console.log("Even Numbers");
 
-// for(let i = 2;i<=10;i=i+2){
-//     console.log(i);
-// }
+for(let i = 2;i<=10;i=i+2){
+    console.log(i);
+}
 
-// console.log("Even Backwards");
+console.log("Even Backwards");
 
-// for(let i = 10;i>=2;i=i-2){
-//     console.log(i);
-// }
+for(let i = 10;i>=2;i=i-2){
+    console.log(i);
+}
 
-// console.log("Table of 5");
-// for(let i = 1;i<=10;i++){
-//     console.log(i*5);
-// }
+console.log("Table of 5");
+for(let i = 1;i<=10;i++){
+    console.log(i*5);
+}
 
-// console.log("Table of Entered No.");
-// let n = prompt("Enter number for getting it's multiplication table:");
-// n = parseInt(n);
-// for(i=n;i>=n*10;i=i+n){
-//     console.log(i);
-// }
+console.log("Table of Entered No.");
+let n = prompt("Enter number for getting it's multiplication table:");
+n = parseInt(n);
+for(i=n;i>=n*10;i=i+n){
+    console.log(i);
+}
 
 // Nested Looping
 for(let i =1;i<=3;i++){
