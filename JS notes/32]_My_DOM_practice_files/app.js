94f973909f3f119d44a74755080bd647bad68071
Raj Rasane <rasaneraj04@gmail.com>
2024-03-24 20:32:07 +0530

    practice files updated

--- JS notes/32]_My_DOM_practice_files/app.js
+++ JS notes/32]_My_DOM_practice_files/app.js
@@ -62,23 +62,23 @@
 
 let btn = document.querySelector("button");
 
-// btn.addEventListener("click",function(){
-//     let h3 = document.querySelector('h3');
-//     h3.innerText = randomcolor();
-//     console.log("Color Updated!");
-//     let div = document.querySelector('div');
-//     div.style.backgroundColor = randomcolor();
-// })
+btn.addEventListener("click",function(){
+    let h3 = document.querySelector('h3');
+    h3.innerText = randomcolor();
+    console.log("Color Updated!");
+    let div = document.querySelector('div');
+    div.style.backgroundColor = randomcolor();
+})
 
 
-// function randomcolor(){
-//     let r = Math.floor(Math.random()*255);
-//     let g = Math.floor(Math.random()*255);
-//     let b = Math.floor(Math.random()*255);
+function randomcolor(){
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
 
-//     let color = `rgb(${r},${g},${b})`;
-//     return color;
-// }
+    let color = `rgb(${r},${g},${b})`;
+    return color;
+}
 
 //---------------------------------------------------------------------------------------------------------------------------------------
 
