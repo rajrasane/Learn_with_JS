d3d2bd98aefa2eae036fffce14dabd42e6cd9b62
Raj Rasane <rasaneraj04@gmail.com>
2024-04-22 12:38:09 +0530

    new notes added

---
+++ JS notes/34]_Promises.js
@@ -0,0 +1,39 @@
+// Before getting towards promises , this things are essential to know :- 
+
+
+
+// JS Single Threaded Nature
+
+// - It refers to that , JS can only execute one task at a time, making it efficient for web browsers.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// JS Asynchronous Nature
+
+// - Asynchronous nature means tasks can happen independently.
+// So, when one task is running, others can start too, instead of waiting for it to finish.
+// This keeps everything moving smoothly, like how you can cook while your laundry is running.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Callback Hell
+
+// - Callback hell occurs when you have multiple nested callbacks in your code. This happens in asynchronous JavaScript programming
+// The problem arises when these callbacks are nested within each other, creating a complex and hard-to-follow structure, like a maze of function calls.
+// This can make your code difficult to understand, debug, and maintain.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// - So to tackle these callback hell problems , we use several asynchronous programming patterns or techniques . In which the first one is :- 
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Promises 
+
+
+// - 
