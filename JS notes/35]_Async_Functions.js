e44610ccf68864ce912a751ee165b29ccd7a29b2
Raj Rasane <rasaneraj04@gmail.com>
2024-04-23 12:43:25 +0530

    notes updated

--- JS notes/35]_Async_Functions.js
+++ JS notes/35]_Async_Functions.js
@@ -65,12 +65,16 @@ async function demo(){
 
 //---------------------------------------------------------------------------------------------------------------------------------------
 
-// for e.g
+// for e.g Handling rejections using try and catch in async
 
 let h1 = document.querySelector("h1");
 
 function colorchange(color,delay){
     return new Promise((resolve, reject) => {
+        let num = Math.floor(Math.random()*5)+1;
+        if(num>4){
+            reject ("Promise rejected!");
+        }
         setTimeout(() => {
             h1.style.color = color;
             console.log("Text color was Changed to ",color);
@@ -80,9 +84,22 @@ function colorchange(color,delay){
 }
 
 async function display(){
-    await colorchange("red",1000);
-    await colorchange("orange",1000);
-    await colorchange("blue",1000);
-    await colorchange("green",1000);
-    colorchange("violet",1000);
+    try{
+        await colorchange("red",1000);
+        await colorchange("orange",1000);
+        await colorchange("blue",1000);
+        await colorchange("green",1000);
+        await colorchange("black",1000);
+    }
+    catch(err){
+        console.log("Error Caught!");
+        console.log(err)
+    }
+
+    let a = 5;
+    console.log(a);
+    console.log("New value : ",a+3);
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
