ce7dc969a059e8c96bd96b6039f4bf9601fd713b
Raj Rasane <rasaneraj04@gmail.com>
2024-04-23 14:56:24 +0530

    notes updated

--- JS notes/36]_API.js
+++ JS notes/36]_API.js
@@ -16,3 +16,33 @@
 
 //---------------------------------------------------------------------------------------------------------------------------------------
 
+// API requesting
+
+// using fetch()
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+        console.log(data.fact);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+// using fetch with async/await
+
+async function getfacts(){
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+    }
+    catch(err){
+        console.log("Error - ",err);
+    }
+}
