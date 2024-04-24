dcbbdbda58635727fb9f93a92c7ca27026c8f796
Raj Rasane <rasaneraj04@gmail.com>
2024-04-24 07:17:02 +0530

    removed unwanted files

--- JS notes/38]_Ajax.js
+++
@@ -1,5 +0,0 @@
-// Ajax (Asynchronous JavaScript and XML)
-
-// - AJAX is a web development trick that helps parts of a webpage change without reloading everything.
-// It lets web browsers talk to servers in the background, so users don't have to wait for the whole page to reload.
-// This makes web pages more lively and responsive.
