646a1bdd4e6888aa8ebd188bb6c7cdf30c4f4881
Raj Rasane <rasaneraj04@gmail.com>
2025-08-05 08:24:21 +0530

    add dialog and popup boxes examples with alert, prompt, and confirm functions; create practicals for addition and input display

---
+++ JS notes/04]_dialog_or_popup_boxes.js
@@ -0,0 +1,23 @@
+// 1. Alert 
+// - displays an alert message on the page
+
+// syntax
+alert("this is a alert");
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// 2. Prompt
+// - displays a dialog box that asks user for some input
+// - usually used to get input from user
+
+// syntax
+prompt("Enter Your Name :");
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// 3. Confirm
+// - display a dialog box that asks input from user with either yes or no
+// - and returns true and false on the basis of the user input
+
+// syntax
+confirm("Do you want to continue :")
